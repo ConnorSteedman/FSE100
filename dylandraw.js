8a6e12c94074b920f1b0307252df4a1014687022
ConnorSteedman <102396419+ConnorSteedman@users.noreply.github.com>
2023-10-19 19:33:37 -0700

    Dylan's Draw File

---
+++ dylandraw.js
@@ -0,0 +1,28 @@
+function DylanDraw() {
+  
+  background('#0d0');
+  
+  text('Memory Game', 400, 120);
+
+  rect(50, 240, 100, 150)
+  
+  rect(200, 240, 100, 150)
+  
+  rect(350, 240, 100, 150)
+  
+  rect(500, 240, 100, 150)
+  
+  rect(650, 240, 100, 150)
+  
+  describe("first row /\ || bottom row \/")
+  
+  rect(50, 400, 100, 150)
+  
+  rect(200, 400, 100, 150)
+  
+  rect(350, 400, 100, 150)
+  
+  rect(500, 400, 100, 150)
+  
+  rect(650, 400, 100, 150)
+}
